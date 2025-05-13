d8b6a9d3e7a5f6142453225895ff5dda9a97fc3f
i.razin <i.razin@megaplan.ru>
2025-05-13 05:45:00 +0300

    Добавил gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/food-diary/",
+  base: "/",
   plugins: [
     react(),
     VitePWA({
